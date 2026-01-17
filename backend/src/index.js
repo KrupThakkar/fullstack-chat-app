25f0259044af830be794462c01fd05370843a8ea
KrupThakkar <krupthakkar.21.cs@iite.indusuni.ac.in>
2026-01-18 00:49:14 +0530

    path error solved

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,7 +9,7 @@ import { app, server } from "./lib/socket.js"
 import path from "path"
 dotenv.config() 
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5001
 const __dirname = path.resolve()
 
 app.use(express.json({ limit: "10mb" }))
@@ -26,9 +26,12 @@ app.use("/api/messages", messageRoutes)
 if(process.env.NODE_ENV ==="production"){
   app.use(express.static(path.join(__dirname, "../frontend/dist")))
 
-  app.get("*", (req, res)=>{
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+ app.get("/*", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "../frontend", "dist", "index.html")
+  )
 })
+
 }
 server.listen(PORT, () => {
   console.log(`Your server has started on port ${PORT}`);
